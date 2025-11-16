2f5f8660a0bf564c3b9587fd7345a73bc30e68e8
Wal-prime <megomorenowal@gmail.com>
2025-11-16 00:48:10 -0500

    Update page.js

--- app/crear/page.js
+++ app/crear/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, Suspense }f rom 'react';
+import { useState, Suspense }from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { db, getAnonymousUser } from '@/lib/firebase';
 import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
